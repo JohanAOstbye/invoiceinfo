bea9e3f269790cd261afa6af963a13284e090462
Johan August Østbye <johan.august@Outlook.com>
2021-05-16 20:32:06 +0200

    added mailing for questions

--- src/components/Form/Areas/InformationArea/informationText.ts
+++ src/components/Form/Areas/InformationArea/informationText.ts
@@ -1,5 +1,9 @@
 export const introText = `
 Denne blanketten bør helst fylles ut av økonomiavdelingen.
-Informasjonen forsikrer at det ikke oppstår feil eller manglende informasjon ved fakturering og betaling.`;
+Informasjonen forsikrer at det ikke oppstår feil eller manglende informasjon ved fakturering og betaling.
+
+Eventuelle spørsmål kan sendes til kontakten deres, evt til [bedkom@online.ntnu.no](mailto:bedkom@online.ntnu.no) eller
+ [fagkom@online.ntnu.no](mailto:fagkom@online.ntnu.no)
+`;
 
 export const productDetails = ` ekstra info`;
