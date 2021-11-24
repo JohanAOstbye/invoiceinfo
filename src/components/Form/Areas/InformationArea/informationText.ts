222c598c37a07d9ca5a574d398b50cbc7c6b839e
Johan August Østbye <Johan.August@outlook.com>
2021-11-24 19:36:43 +0100

    build was added in wrong repo

--- src/components/Form/Areas/InformationArea/informationText.ts
+++ src/components/Form/Areas/InformationArea/informationText.ts
@@ -1,8 +1,9 @@
 export const introText = `
-Denne blanketten bør helst fylles ut av økonomiavdelingen.
+Denne blanketten bør helst fylles ut av __økonomiavdelingen__.
 Informasjonen forsikrer at det ikke oppstår feil eller manglende informasjon ved fakturering og betaling.
 
-Eventuelle spørsmål kan sendes til kontakten deres, evt til [bedkom@online.ntnu.no](mailto:bedkom@online.ntnu.no) eller
+Eventuelle spørsmål kan sendes til kontakten deres, evt til [bankom@online.ntnu.no](mailto:bankom@online.ntnu.no),
+[bedkom@online.ntnu.no](mailto:bedkom@online.ntnu.no) eller
  [fagkom@online.ntnu.no](mailto:fagkom@online.ntnu.no)
 `;
 
